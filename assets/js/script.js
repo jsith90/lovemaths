d909c2564642a1303594cf750fe1a3fabfef2b19
Josh Sinclair <j.sinclairthomson@gmail.com>
2024-01-25 12:38:19 +0000

    Add event listeners for document loading and button click

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,18 @@
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
+    // also can use (let= i = 0; i < buttons.length; i++) in the for loop below
+    for (let button of buttons){
+      button.addEventListener("click", function() {
+        if (this.getAttribute("data-type") === "submit") {
+            alert("You clicked Submit!");
+        } else {
+            let gameType = this.getAttribute("data-type");
+            alert(`You clicked ${gameType}`);
+        }
+      })  
+    }
+})
+
 function runGame() {
 
 }
